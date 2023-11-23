ad34b4841ce11ac9cd2ad4cc1f49d50027a639dc
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-11-23 13:30:18 +0700

    feat(exercise): add completed exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalProductPrice = 27000;
+let percentDiscount = 25;
+let productPriceAfterDiscount = (totalProductPrice * percentDiscount) / 100;
