aa7278e3a7de43be29040d2a85175db65c313b23
Gravizz <peeke0rr@gmail.com>
2023-11-23 14:00:41 +0700

    feat(ex):complete exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let Discount = 0.25;
+let netTotalPrice = totalPrice - totalPrice * Discount;
--- ex-6.js
+++ ex-6.js
@@ -4,3 +4,9 @@ let totalParts = 13000;
 
 let concatSentence;
 let literalSentence;
+
+concatSentence = `Total: ` + totalParts + ` Machine part`;
+literalSentence = `Total: ${totalParts} Machine part`;
+
+console.log(concatSentence);
+console.log(literalSentence);
